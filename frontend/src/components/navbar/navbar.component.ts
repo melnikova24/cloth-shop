2a8f58882224022e336e6fb6668fb6d683488a6c
melnikova24 <quillnata24@yandex.ru>
2024-03-28 14:49:30 +0300

    Изменены иконки в навбаре

--- frontend/src/components/navbar/navbar.component.ts
+++ frontend/src/components/navbar/navbar.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {Router, RouterLink} from "@angular/router";
+
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
-  constructor(private router: Router) { }
 
-  redirectToCatalog(filter: string) {
-    this.router.navigate(['/catalog'], { queryParams: { filter: filter } });
-  }
 }
