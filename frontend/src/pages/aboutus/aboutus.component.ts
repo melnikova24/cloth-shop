83021a00ea79ab2c08a63120befc5a190ba7a79c
melnikova24 <quillnata24@yandex.ru>
2024-05-12 23:09:43 +0300

    Страница с информацией о бренде

---
+++ frontend/src/pages/aboutus/aboutus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-aboutus',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './aboutus.component.html',
+  styleUrl: './aboutus.component.scss'
+})
+export class AboutusComponent {}
