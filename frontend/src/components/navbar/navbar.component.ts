a637f2275021e5fc5eec011f7fb15e556e9122b7
melnikova24 <quillnata24@yandex.ru>
2024-05-13 15:21:27 +0300

    Адаптивное меню навигации

--- frontend/src/components/navbar/navbar.component.ts
+++ frontend/src/components/navbar/navbar.component.ts
@@ -2,21 +2,33 @@ import { Component } from '@angular/core';
 
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {AuthService} from "../../shared/api/auth";
-import {NgIf} from "@angular/common";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, NgIf, RouterLinkActive],
+  imports: [RouterLink, NgIf, RouterLinkActive, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  isOpen = false
   constructor(private authService: AuthService) {
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen
+  }
+
+  logout() {
+    this.authService.logout()
   }
 
   get isAdmin () {
     return this.authService.isAdmin();
+  }
+
+  get isAuth() {
+    return this.authService.isLoggedIn;
   }
 }
